b22b1da7675a5650979bd74a3e1dbb49e1d7e674
Rohit Lohia <rohitlohia211@gmail.com>
2020-07-11 14:53:46 +0530

    add notes functionality added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import reacticon from './images/react.png'
 import bootstrap from './images/bootstrap.jpg'
 import google from "./images/google.png"
+import Notes from './Notes.jsx'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
 
 
 
 function App() {
+
+  const [data, updateData] = useState({
+    title: "",
+    content: ""
+
+  }
+  )
+
+  const [notes, updateNotes] = useState([])
+
+  const handleChange = (e) => {
+    updateData({
+      ...data,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleClick = () => {
+
+    updateNotes([...notes, data])
+  }
+
 
   return (
     <>
@@ -27,16 +50,26 @@ function App() {
 
         {/* User Input Area (Note Title and Body) */}
 
-        <div className="input">
+        <div className="input my-5">
 
           <form>
 
-            <input type='text' placeholder="Title.." />
-            <textarea name='content' placeholder="Take a note..." spellcheck="false"></textarea>
-            <button type="button" class="btn btn-success addbtn"><span>+</span></button>
+            <input name="title" value={data.title} type='text' placeholder="Title.." onChange={handleChange} />
+            <textarea name='content' value={data.content} placeholder="Take a note..." spellcheck="false" onChange={handleChange}></textarea>
+            <button type="button" class="btn btn-success addbtn" onClick={handleClick}><span>+</span></button>
 
 
           </form>
+
+        </div>
+        {/* User Input Area Ends (Note Title and Body) */}
+
+        <div className="notes">
+          {notes.map((value) => {
+            return <Notes title={value.title} content={value.content} />
+          })}
+
+
 
         </div>
 
