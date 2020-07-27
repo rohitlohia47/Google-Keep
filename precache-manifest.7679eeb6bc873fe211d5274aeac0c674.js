565c76a5a71f88a931ed03997b45ea9bf4566c58
rohitlohia <rohitlohia211@gmail.com>
2020-07-27 12:03:12 +0530

    Updates

---
+++ precache-manifest.7679eeb6bc873fe211d5274aeac0c674.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1593f2a38801d456374bfd2bc3298117",
+    "url": "/Google-Keep/index.html"
+  },
+  {
+    "revision": "3d7b6b35667ce1f9c900",
+    "url": "/Google-Keep/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "3d3c4d78f12340c6b12f",
+    "url": "/Google-Keep/static/css/main.444487fb.chunk.css"
+  },
+  {
+    "revision": "3d7b6b35667ce1f9c900",
+    "url": "/Google-Keep/static/js/2.b30365ad.chunk.js"
+  },
+  {
+    "revision": "9e81e617707513432f3da640fbf3bd82",
+    "url": "/Google-Keep/static/js/2.b30365ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3c4d78f12340c6b12f",
+    "url": "/Google-Keep/static/js/main.fb6c7fc7.chunk.js"
+  },
+  {
+    "revision": "381bbdc909e4d3f757e5",
+    "url": "/Google-Keep/static/js/runtime-main.0ec1a971.js"
+  },
+  {
+    "revision": "1aa61eee95bff5d7bacae56058a9b2a1",
+    "url": "/Google-Keep/static/media/bootstrap.1aa61eee.jpg"
+  },
+  {
+    "revision": "0a6783bd7cec463316492318e90740d5",
+    "url": "/Google-Keep/static/media/google.0a6783bd.png"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/Google-Keep/static/media/react.37fe8322.png"
+  }
+]);
